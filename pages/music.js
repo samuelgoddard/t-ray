ea08892c1529e5e160908bc8934b06355046687c
Sam Goddard <hello@samgoddard.co.uk>
2022-02-22 15:31:36 +0000

    seo image

--- pages/music.js
+++ pages/music.js
@@ -77,7 +77,7 @@ export default function Music(initialData) {
           description: 'T-Ray Armstrong, aka IAmReallyATrex, is a drummer and musician, born on May 31, 1993 in Barbados. He began his career playing with the Barbadian band Cover Drive in 2010.',
           images: [
             {
-              url: '/images/social.jpg',
+              url: 'https://www.imreallyatrex.com/static/social.jpg',
               width: 1200,
               height: 630,
               alt: `ImReallyATRex | T-Ray Logo`,
