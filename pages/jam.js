c4206d0687f4f1d7f5ffd8ffb9ac0c6eff36b42f
Sam Goddard <hello@samgoddard.co.uk>
2022-02-22 15:36:20 +0000

    seo image

--- pages/jam.js
+++ pages/jam.js
@@ -19,23 +19,6 @@ export default function Jam() {
       <NextSeo
         title="Jam"
         description="T-Ray Armstrong, aka IAmReallyATrex, is a drummer and musician, born on May 31, 1993 in Barbados. He began his career playing with the Barbadian band..."
-        openGraph={{
-          url: 'https://imreallyatrex.com/jam',
-          title: `Jam | ImReallyATRex`,
-          description: 'T-Ray Armstrong, aka IAmReallyATrex, is a drummer and musician, born on May 31, 1993 in Barbados. He began his career playing with the Barbadian band...',
-          images: [
-            {
-              url: 'https://www.imreallyatrex.com/static/social.jpg',
-              width: 1200,
-              height: 630,
-              alt: `ImReallyATRex | T-Ray Logo`,
-            },
-          ],
-          site_name: `ImReallyATRex`,
-        }}
-        twitter={{
-          cardType: 'summary_large_image',
-        }}
       />
       
       <LazyMotion features={domMax}>
